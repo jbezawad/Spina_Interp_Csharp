de03bb40ac09bda006ab47fbaaa5d60fd819db9e
JAGADISH PRADEEP BEZAWADA <jbezawad@syr.edu>
2009-10-11 15:26:11 -0400

    2nd commit parallel_for

---
+++ InterpEdited/Interp_CSharp/Parallel_For.cs
@@ -0,0 +1,29 @@
+﻿////////////////////////////////////////////////////////////////////////
+// AssignmentOperationElement.cs: holds the data needed for an 
+//  assignment operation.
+// 
+// version: 1.0
+// description: part of the interpreter example for the visitor design
+//  pattern.
+// author: phil pratt-szeliga (pcpratts@syr.edu)
+// language: C# .Net 3.5
+////////////////////////////////////////////////////////////////////////
+
+public class Parallel_For : Element
+{
+
+    LoopElement mLhs;
+    Element mRhs;
+
+    public override void Accept(Visitor visitor)
+    {
+        visitor.VisitParallelForElement(this);
+    }
+    
+    public Element getLhs() { return mLhs; }
+    public void setLhs(LoopElement lhs) { mLhs = lhs; }
+    
+    public Element getRhs() { return mRhs; }
+    public void setRhs(Element rhs) { mRhs = rhs; }
+
+}
